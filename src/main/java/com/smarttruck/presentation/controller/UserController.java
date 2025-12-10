da6e0ed02c724a7e8e3ebce248ce800500eab7d7
pedrodev03 <pedroaugustor03@gmail.com>
2025-12-10 18:07:37 -0300

    alterações para conectar novamente com o front2;

--- src/main/java/com/smarttruck/presentation/controller/UserController.java
+++ src/main/java/com/smarttruck/presentation/controller/UserController.java
@@ -48,23 +48,8 @@ public class UserController {
 
         CreateUserResponse response = UserMapper.toResponse(user);
         return ResponseEntity.ok(response);
-
     }
-    /*
-    @PutMapping("/{id}")
-    public ResponseEntity<CreateUserResponse> update(
-        @PathVariable UUID id,
-        @Valid @RequestBody UpdateUserRequest request) {
 
-        // Chama o caso de uso para atualizar
-        User updatedUser = updateUserUseCase.execute(
-            id,
-            request.name(),
-            request.email(),
-            request.phone()
-        );
-
-         */
     /**
      * Lista usuários ativos com paginação.
      *
